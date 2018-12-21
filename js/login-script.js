f05a29fc3d3d33e44b5d1ae262c4e3e78b463766
Albert Omware <aomware@gmail.com>
2018-12-21 11:51:53 +0300

    made changes to login script and index file

--- js/login-script.js
+++ js/login-script.js
@@ -1,43 +1,36 @@
-document.ready(function() {
-  $('#login-form').submit(function(event) {
-    event.preventdefault();
-    var fName = $("#firstname-id").val();
-    var lName = $("#lastname-id").val();
-    var phone = $("#phone-id").val();
-    var email = $("#email-id").val();
-    var password = $("#password-id").val();
+function store() {
+  var inputfName = document.getElementById("#fName");
+  localStorage.setItem("fName", inputfName.value);
+  var inputlName = document.getElementById("#lName");
+  localStorage.setItem("lName", inputlName.value);
+  var inputphone = document.getElementById("#phone");
+  localStorage.setItem("phone", inputphone.value);
+  var inputemail = document.getElementById("#email");
+  localStorage.setItem("email", inputemail.value);
+  var inputpassword = document.getElementById("#password");
+  localStorage.setItem("password", inputpassword.value);
+}
+var storedemail;
+var storedfname;
+var storedlname;
+var storedphone;
+var storedpassword;
+// check if stored data from sign_up-form is equal to entered data in the login-form
+function check() {
+  // stored data from the sign_up-form
+  storedfName = localStorage.getItem('#fName');
+  storedlName = localStorage.getItem('#lName');
+  storedphone = localStorage.getItem('#phone');
+  storedemail = localStorage.getItem('#email');
+  storedpassword = localStorage.getItem('#password');
+}
+var useremail = document.getElementById('#email-id');
+var userpassword = document.getElementById('#password-id');
 
-    // storing input from sign_up-form
-    function store() {
-      localStorage.setItem('fName', firstname.value);
-      localStorage.setItem('lName', lastname.value);
-      localStorage.setItem('email', email.value);
-      localStorage.setItem('password', password.value);
-      localStorage.setItem('phone', phone.value);
-
-    }
-
-    // check if stored data from sign_up-form is equal to entered data in the login-form
-    function check() {
-
-      // stored data from the sign_up-form
-      var storedfName = localStorage.getItem('firstname');
-      var storedlName = localStorage.getItem('lastname');
-      var storedphone = localStorage.getItem('phone');
-      var storedemail = localStorage.getItem('email');
-      var storedpassword = localStorage.getItem('password');
-
-      // entered data from the login-form
-      var useremail = document.getElementById('useremail');
-      var userpassword = document.getElementById('userpassword');
-
-      // check if stored data from sign_up-form is equal to data from login form
-      if(useremail.value == storedemail && userpassword.value == storedpassword) {
-        alert('You are loged in.');
-    }else {
-        alert('Invalid login credentials!!!');
-    }
-      }
-    }
-  });
-});
+// check if stored data from login form
+if(useremail == storedemail && userpassword == storedpassword) {
+alert('You are logged in!!!');
+}
+else {
+  alert('Invalid login credentials!!!');
+}
